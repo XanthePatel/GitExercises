f1b7cdacf55190023b6e71f6b1036560e796f6df
pengxingyi <pengxingyi@mj.com>
2024-04-24 14:43:42 +0800

    parent ed6f2db5a81126dd62fae50ecacc920b023c2364 author pengxingyi <pengxingyi@mj.com> 1713941022 +0800 committer pengxingyi <pengxingyi@mj.com> 1713943441 +0800
    feat: 新增./output

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = 4;
+
+    int sum = 0;
+    sum = a + b;
+
+    cout << "a + b = " << sum << endl;
+    return 0; 
+}
